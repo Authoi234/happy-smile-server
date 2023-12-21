0fe4ab2043b431c4f04e0f77a93bb9e9facaa68d
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-21 12:16:21 +0630

    test is success

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6iupoas.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {

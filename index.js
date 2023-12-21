457f2ed14445b53c7b2b071283e267b6bb4951ba
Authoi234 <jawadkhan.authoi@gmail.com>
2023-12-21 12:13:48 +0630

    all thing is installed and basic structure is ready

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const mongodb = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+// creating app
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// connecting to the server
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.6iupoas.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+  } finally {
+
+  }
+}
+run().catch(err => console.log(err));
+
+
+app.get('/', (req, res) => {
+    res.send('Happy smile server')
+});
+
+app.listen(port, () => {
+    console.log('Happy smile server is running on port', port);
+})
